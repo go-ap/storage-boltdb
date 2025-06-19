d998019b7da3dc0a7ca1cbde88306d515f8709b7
Marius Orcsik <marius@federated.id>
2025-06-19 15:48:03 +0300

    Updated Load/Store Metadata methods with new signature
    We now accept dynamic metadata being passed from the calling code.
    
    The only invisible requirement is that the data type can be
    encoded/decoded by the encode/decode static functions.

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.23.0
 
 require (
 	github.com/go-ap/activitypub v0.0.0-20250527110644-1410ed93404d
-	github.com/go-ap/auth v0.0.0-20250527112020-6b6fbf0ccd0a
 	github.com/go-ap/errors v0.0.0-20250527110557-c8db454e53fd
 	github.com/go-ap/filters v0.0.0-20250527111509-8ef063ce5449
 	github.com/openshift/osin v1.0.2-0.20220317075346-0f4d38c6e53f
@@ -14,22 +13,14 @@ require (
 
 require (
 	git.sr.ht/~mariusor/go-xsd-duration v0.0.0-20220703122237-02e73435a078 // indirect
-	git.sr.ht/~mariusor/lw v0.0.0-20250325163623-1639f3fb0e0d // indirect
-	git.sr.ht/~mariusor/mask v0.0.0-20250114195353-98705a6977b7 // indirect
 	github.com/RoaringBitmap/roaring v1.9.4 // indirect
 	github.com/bits-and-blooms/bitset v1.22.0 // indirect
 	github.com/go-ap/jsonld v0.0.0-20221030091449-f2a191312c73 // indirect
-	github.com/go-chi/chi/v5 v5.2.1 // indirect
-	github.com/go-fed/httpsig v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jdkato/prose v1.2.1 // indirect
 	github.com/mariusor/qstring v0.0.0-20200204164351-5a99d46de39d // indirect
-	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mschoch/smat v0.2.0 // indirect
 	github.com/pborman/uuid v1.2.1 // indirect
-	github.com/rs/xid v1.6.0 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/valyala/fastjson v1.6.4 // indirect
 	golang.org/x/sys v0.33.0 // indirect
--- repository.go
+++ repository.go
@@ -2,30 +2,19 @@ package boltdb
 
 import (
 	"bytes"
-	"crypto"
-	"crypto/dsa"
-	"crypto/ecdsa"
-	"crypto/ed25519"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
 	"os"
 	"path/filepath"
 	"time"
 
 	vocab "github.com/go-ap/activitypub"
-	au "github.com/go-ap/auth"
 	"github.com/go-ap/errors"
 	"github.com/go-ap/filters"
 	bolt "go.etcd.io/bbolt"
-	"golang.org/x/crypto/bcrypt"
 )
 
 var encodeItemFn = vocab.MarshalJSON
 var decodeItemFn = vocab.UnmarshalJSON
-
-type Metadata = au.Metadata
 
 type repo struct {
 	d     *bolt.DB
@@ -797,156 +786,6 @@ func (r *repo) close() error {
 	return err
 }
 
-// PasswordSet
-func (r *repo) PasswordSet(it vocab.Item, pw []byte) error {
-	path := itemBucketPath(it.GetLink())
-
-	err := r.d.Update(func(tx *bolt.Tx) error {
-		root, err := tx.CreateBucketIfNotExists(r.root)
-		if err != nil {
-			return errors.Errorf("Not able to write to root bucket %s", r.root)
-		}
-		if root == nil {
-			return ErrorInvalidRoot(r.root)
-		}
-		if !root.Writable() {
-			return errors.Errorf("Non writeable bucket %s", r.root)
-		}
-		var b *bolt.Bucket
-		b, _, err = descendInBucket(root, path, true)
-		if err != nil {
-			return errors.Newf("Unable to find %s in root bucket", path)
-		}
-		if !b.Writable() {
-			return errors.Errorf("Non writeable bucket %s", path)
-		}
-
-		pw, err = bcrypt.GenerateFromPassword(pw, -1)
-		if err != nil {
-			return errors.Annotatef(err, "Could not encrypt the pw")
-		}
-		m := Metadata{
-			Pw: pw,
-		}
-		entryBytes, err := encodeFn(m)
-		if err != nil {
-			return errors.Annotatef(err, "Could not marshal metadata")
-		}
-		err = b.Put([]byte(metaDataKey), entryBytes)
-		if err != nil {
-			return errors.Errorf("Could not insert entry: %s", err)
-		}
-		return nil
-	})
-
-	return err
-}
-
-// PasswordCheck
-func (r *repo) PasswordCheck(it vocab.Item, pw []byte) error {
-	path := itemBucketPath(it.GetLink())
-	m := Metadata{}
-	err := r.d.View(func(tx *bolt.Tx) error {
-		root := tx.Bucket(r.root)
-		if root == nil {
-			return ErrorInvalidRoot(r.root)
-		}
-		var b *bolt.Bucket
-		var err error
-		b, path, err = descendInBucket(root, path, false)
-		if err != nil {
-			return errors.Newf("Unable to find %s in root bucket", path)
-		}
-		entryBytes := b.Get([]byte(metaDataKey))
-		err = decodeFn(entryBytes, &m)
-		if err != nil {
-			return errors.Annotatef(err, "Could not unmarshal metadata")
-		}
-		if err := bcrypt.CompareHashAndPassword(m.Pw, pw); err != nil {
-			return errors.NewUnauthorized(err, "Invalid pw")
-		}
-		return nil
-	})
-	return err
-}
-
-// LoadMetadata
-func (r *repo) LoadMetadata(iri vocab.IRI) (*Metadata, error) {
-	path := itemBucketPath(iri)
-
-	var m *Metadata
-	err := r.d.View(func(tx *bolt.Tx) error {
-		root := tx.Bucket(r.root)
-		if root == nil {
-			return ErrorInvalidRoot(r.root)
-		}
-		var b *bolt.Bucket
-		var err error
-		b, path, err = descendInBucket(root, path, false)
-		if err != nil {
-			return errors.Newf("Unable to find %s in root bucket", path)
-		}
-		entryBytes := b.Get([]byte(metaDataKey))
-		m = new(Metadata)
-		return decodeFn(entryBytes, m)
-	})
-	return m, err
-}
-
-// SaveMetadata
-func (r *repo) SaveMetadata(m Metadata, iri vocab.IRI) error {
-	path := itemBucketPath(iri)
-	err := r.d.Update(func(tx *bolt.Tx) error {
-		root, err := tx.CreateBucketIfNotExists(r.root)
-		if err != nil {
-			return errors.Errorf("Not able to write to root bucket %s", r.root)
-		}
-		if root == nil {
-			return ErrorInvalidRoot(r.root)
-		}
-		if !root.Writable() {
-			return errors.Errorf("Non writeable bucket %s", r.root)
-		}
-		var b *bolt.Bucket
-		b, _, err = descendInBucket(root, path, true)
-		if err != nil {
-			return errors.Newf("Unable to find %s in root bucket", path)
-		}
-		if !b.Writable() {
-			return errors.Errorf("Non writeable bucket %s", path)
-		}
-
-		entryBytes, err := encodeFn(m)
-		if err != nil {
-			return errors.Annotatef(err, "Could not marshal metadata")
-		}
-		err = b.Put([]byte(metaDataKey), entryBytes)
-		if err != nil {
-			return errors.Errorf("Could not insert entry: %s", err)
-		}
-		return nil
-	})
-
-	return err
-}
-
-// LoadKey loads a private key for an actor found by its IRI
-func (r *repo) LoadKey(iri vocab.IRI) (crypto.PrivateKey, error) {
-	m, err := r.LoadMetadata(iri)
-	if err != nil {
-		return nil, err
-	}
-	b, _ := pem.Decode(m.PrivateKey)
-	if b == nil {
-		return nil, errors.Errorf("failed decoding pem")
-	}
-	prvKey, err := x509.ParsePKCS8PrivateKey(b.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	return prvKey, nil
-}
-
 func Path(c Config) (string, error) {
 	if !filepath.IsAbs(c.Path) {
 		c.Path, _ = filepath.Abs(c.Path)
@@ -973,77 +812,4 @@ func mkDirIfNotExists(p string) error {
 		return errors.Errorf("path exists, and is not a folder %s", p)
 	}
 	return nil
-}
-
-// SaveKey saves a private key for an actor found by its IRI
-func (r *repo) SaveKey(iri vocab.IRI, key crypto.PrivateKey) (vocab.Item, error) {
-	ob, err := r.loadOneFromBucket(iri)
-	if err != nil {
-		return nil, err
-	}
-
-	typ := ob.GetType()
-	if !vocab.ActorTypes.Contains(typ) {
-		return ob, errors.Newf("trying to generate keys for invalid ActivityPub object type: %s", typ)
-	}
-	actor, err := vocab.ToActor(ob)
-	if err != nil {
-		return ob, errors.Newf("trying to generate keys for invalid ActivityPub object type: %s", typ)
-	}
-
-	m, err := r.LoadMetadata(iri)
-	if err != nil && !errors.IsNotFound(err) {
-		return ob, err
-	}
-	if m == nil {
-		m = new(Metadata)
-	}
-	if m.PrivateKey != nil {
-		r.logFn("actor %s already has a private key", iri)
-	}
-	prvEnc, err := x509.MarshalPKCS8PrivateKey(key)
-	if err != nil {
-		r.errFn("unable to x509.MarshalPKCS8PrivateKey() the private key %T for %s", key, iri)
-		return ob, err
-	}
-
-	m.PrivateKey = pem.EncodeToMemory(&pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: prvEnc,
-	})
-	if err = r.SaveMetadata(*m, iri); err != nil {
-		r.errFn("unable to save the private key %T for %s", key, iri)
-		return ob, err
-	}
-
-	var pub crypto.PublicKey
-	switch prv := key.(type) {
-	case *ecdsa.PrivateKey:
-		pub = prv.Public()
-	case *rsa.PrivateKey:
-		pub = prv.Public()
-	case *dsa.PrivateKey:
-		pub = &prv.PublicKey
-	case *ed25519.PrivateKey:
-		pub = prv.Public()
-	default:
-		r.errFn("received key %T does not match any of the known private key types", key)
-		return ob, nil
-	}
-	pubEnc, err := x509.MarshalPKIXPublicKey(pub)
-	if err != nil {
-		r.errFn("unable to x509.MarshalPKIXPublicKey() the private key %T for %s", pub, iri)
-		return ob, err
-	}
-	pubEncoded := pem.EncodeToMemory(&pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: pubEnc,
-	})
-
-	actor.PublicKey = vocab.PublicKey{
-		ID:           vocab.IRI(fmt.Sprintf("%s#main", iri)),
-		Owner:        iri,
-		PublicKeyPem: string(pubEncoded),
-	}
-	return r.Save(actor)
 }
